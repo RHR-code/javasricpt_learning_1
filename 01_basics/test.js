d78db59ef9bdfbb59530890b2134531bfdd6d2fa
RHR-code <123375387+RHR-code@users.noreply.github.com>
2024-01-07 14:18:03 +0000

    Math notes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,27 @@
-console.log("Ratul")
+
+
+// let othernum = 1234.8948;
+
+// console.log(othernum.toPrecision(6));
+
+// let num = 1000000;
+
+// console.log(num.toLocaleString("en-IN"));
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.45345));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.sqrt(25));
+
+
+// console.log(Math.round(Math.floor((Math.random() * 10) +1)));
+
+let min = 10;
+let max = 20;
+
+
+
+
+console.log(Math.floor((Math.random() * (max - min) + 1) + min ))
